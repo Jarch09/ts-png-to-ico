c0f9ee45db7ce80eec5defa6fa66e257b9c9ba8c
jarch09 <jarch09@gmail.com>
2022-09-28 20:13:53 -0400

    updating package name

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# png-to-ico
+# ts-png-to-ico
 
 Convert PNG to ICO in memory.
 
 This repo serves as a replacement for [https://github.com/kevva/to-ico](https://github.com/kevva/to-ico).
+
---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+THIS_DIR=$(pwd)
+
+# ----------------------------------
+# PUBLISH
+# ----------------------------------
+
+# Publish package
+echo "* Preparing to publish on NPM..."
+
+npm publish
+if [[ "$?" -ne "0" ]]; then
+  echo "Failed to publish package";
+  exit 1;
+fi
+
+echo "---"
+echo "* Published!"
+exit 0;
